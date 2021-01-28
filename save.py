57ab6f11d515ec689d8a5c6d5e6c5247df134b16
Sử Thành Công <stc17022002@gmail.com>
2021-01-28 23:38:10 +0700

    khók ĩa

--- save.py
+++ save.py
@@ -1,45 +1,40 @@
-#Các thư viện cần thiết:
-import os, re
+# Các thư viện cần thiết:
+import os
+import re
 
-#Các hàm:
 
-
+# Các hàm:
 # Hàm tạo thư mục:
-def creat_dir(folder):
+def creat_dir():
     """
-    Tạo và chuyển vị trí lưu đến thư mục vừa tạo (Thuộc ổ đĩa C)
+    Tạo và chuyển vị trí lưu đến thư mục vừa tạo
     : directory:
     """
     while True:
-        if not os.path.exists('C:\\'+folder):
-            os.mkdir('C:\\'+folder)
+        folder = input('Nhập tên thư mục bạn muốn tạo:')
+        if not os.path.exists(folder):
+            os.mkdir(folder)
             print("Folder '{}' đã được tạo.".format(folder))
-            os.chdir('C:\\' + folder)
+            os.chdir(folder)
             print('-' * 50)
             break
         else:
             check = input("Folder '{}' tồn tại. Bạn có muốn lưu tại Folder này không? (y/n) ".format(folder))
             if check == 'y' or check == 'Y':
                 print("Đã chuyển vị trí lưu đến thư mục '{}'.".format(folder))
-                os.chdir('C:\\' + folder)
+                os.chdir(folder)
                 print('-' * 50)
                 break
-            else:
-                print('Vui lòng nhập tên thư mục:')
-
-
-#Hàm tạo tên file html tự động (vd:"filename_index.html")
-# Hàm tạo tên file tự động
-def file_name(url,index):
-    url = re.sub("^(https://www.|http://www.|https://|http://)", "", url)
-    url = re.sub('\W', "", url)
-    url = re.sub('(com)?',"",url)
-    name = url + '_' + str(index)
-    return name
 
 # Creat a new file
-def write_file(path, data):
-    if not os.path.isfile(path+'.html'):
-        f = open(path +'.html', 'wb')
+def write_file(name, data):
+    if not os.path.isfile(name + '.html'):
+        f = open(name + '.html', 'wb')
         f.write(data)
         f.close()
+
+
+def write_data(url):
+    f = open('0_DownloadedLink.txt', 'a+')
+    f.write(url+'\n')
+    f.close()
--- web_op.py
+++ web_op.py
@@ -1,54 +1,94 @@
-#Các thư viện cần thiết:
+# Các thư viện cần thiết:
 import requests
 from bs4 import BeautifulSoup
-import re
+import re, csv
 import urllib.request, urllib.error, urllib.parse
+from requests import HTTPError
+#from urllib import request
 
-#Các hàm cần thiết:
-#Hàm đọc nội dung trang web:
+
+# Các hàm cần thiết:
+# Nhập số lượng trang web cần tải về:
+def input_max():
+    while True:
+        try:
+            num = int(input('Số lượng trang web cần tải về:'))
+        except ValueError:
+            print('Không phải là số tự nhiên, vui lòng nhập lại!\n' + '-' * 5)
+            continue
+        else:
+            if num > 0:
+                return num
+            else:
+                print('Hãy nhập lại số tự nhiên lớn hơn 0.')
+
+
+# Nhập url:
+def input_url():
+    pattern = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    domain = input('Nhập url xuất phát:')
+    while re.match(pattern, domain) is None:
+        domain = input('Url không hợp lệ, vui lòng nhập lại:')
+        print('-' * 5)
+    if domain[-1] == '/':
+        domain = domain[0: -1]
+        return domain
+    else:
+        return domain
+
+
+# Hàm đọc nội dung trang web:
 def save_content(url):
     response = urllib.request.urlopen(url)
     result = response.read()
     return result
 
 
-#Kết quả trả về là 1 văn bản dạng chuỗi .txt
+# Kết quả trả về là 1 văn bản dạng chuỗi .txt
 def parsing(url):
     # Gửi yêu cầu truy cập url
     try:
         raw_page = requests.get(url)
-    except HTTPError as e:
-        print("The server returned an HTTP error")
-    except URLError as e:
-        print("The server could not be found!")
+    except HTTPError:
+        print("The server returned an HTTP error:")
     else:
-        raw_page = requests.get(url)
-    # Lấy code html của trang web được trả về theo url
+        # Lấy code html của trang web được trả về theo url
         result = BeautifulSoup(raw_page.text, "html.parser")
         return result
 
 
-#Lấy các đường link web trong nội dung đọc về:
-#Kết quả trả về là 1 list chứa các link
-def catchlink(page_content):
+# Lấy các đường link web trong nội dung đọc về:
+# Kết quả trả về là 1 list chứa các link
+def find_link(page_content, domain):
     """
     Lấy các đường link từ nội dung trang web
-    :param page_content:
+    :param page_content:nội dung của trang
+    :param domain:url ban đầu
     :return: List (chứa các link)
     """
     a_tags = page_content.find_all("a")
-    result = []
+    result = set()
     for item in a_tags:
         link = item.get("href")
-        result.append(link)
+        if checkHTTP(link, domain):
+            result.add(link)
+        elif check_link(link):
+            result.add(domain + link)
     return result
 
 
 # Hàm kiểm tra link có chưa https/http hay không:
 # Kết quả trả về:True nếu hợp lệ/False nếu không hợp lệ
-def checkHTTP(item):
+def checkHTTP(item, domain):
     link = str(item)
-    regex = re.compile(r'^(http://|https://)')
+    domain = str(domain)
+    regex = re.compile(f'^({domain}).*')
     result = re.match(regex, link) is not None
     return result
 
@@ -57,19 +97,33 @@ def checkHTTP(item):
 # Kết quả trả về True/False
 def check_link(item):
     link = str(item)
-    regex = re.compile(r'^/\w/')
+    # regex = re.compile(r'^/.*(html|epi)$?')
+    regex = '^/.*(html|api|[/])$'
     result = re.match(regex, link) is not None
     return result
 
 
-#Hàm chỉnh sửa đường link nếu đường link không đầy đủ:
-#Kết quả trả về là 1 đường link đầy đủ.
-def editlink(domain,item):
-    """
-    Hàm chỉnh sửa đường link bị thiếu tên miền
-    :param domain: Tên miền
-    :param item: Đường link bị thiếu
-    :return:
-    """
-    item = domain + item
-    return str(item)
+# Hàm tạo tên file html tự động (vd:"filename_index.html")
+# Hàm tạo tên file tự động
+def file_name(domain, url, index):
+    a = len(domain) + 1
+    if '.html' in url:
+        name = getTitle(url)
+    else:
+        if url == domain:
+            name = 'TRANG CHỦ'
+        else:
+            #name = url[a:]
+            name = re.sub(domain,'MỤC',url)
+    name = re.sub('\W','_',name)
+    name = str(index) + '_' + name
+    return name
+
+
+# Hàm lấy tiêu đề bài viết
+def getTitle(url):
+    html = urllib.request.urlopen(url).read().decode('utf8')
+    html[:60]
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('title')
+    return str(title.string)
